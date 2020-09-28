51993932b897affb38db0ab33a977e102e84502d
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2020-09-28 19:22:25 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,19 @@
+import csv
+import re
+symbols=int()
+spaces=int()
+pun_sym=int()
+words=int()
+sent=int()
+with open('steam_description_data.csv') as f:
+    r=csv.reader(f)
+    for line in r:
+        symbols+=len(line)
+        spaces+=line.count(' ')
+        pun_sym+=line.count('.')+line.count(',')+line.count('?')
+        pun_sym+=line.count('!')+line.count('-')+line.count(':')
+        pun_sym+=line.count(';')+line.count('(')+line.count(')')
+        words=len(re.findall("(\w+-\w+)|(\w+'\w+)|(\w+-\w+'\w+)|(\w+)",line))
+        
+        sent=len(re.findall("([A-ZА-ЯЁ][^\.!?]*[\.!?])",line))
+print(symbols,symbols-spaces,symbols-pun_sym,words,sent)
