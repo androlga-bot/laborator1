f1568b7752d3eaf6bf223f3c03fb76e7ef42a84e
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2020-12-09 01:14:12 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,21 +1,21 @@
 import csv
 import re
-symbols=0
-spaces=0
-pun_sym=0
-words=0
-sent=0
-picture=0
-with open('steam_description_data.csv',encoding='utf-8') as f:
+symbols = 0
+spaces = 0
+pun_sym = 0
+words = 0
+sent = 0
+picture = 0
+with open('steam_description_data.csv', encoding='utf-8') as f:
     r=csv.reader(f)
     for l in r:
-        line=','.join(l)
-        symbols+=len(line)
-        spaces+=line.count(' ')
-        pun_sym+=line.count('.')+line.count(',')+line.count('?')
-        pun_sym+=line.count('!')+line.count('-')+line.count(':')
-        pun_sym+=line.count(';')+line.count('(')+line.count(')')
-        words+=len(re.findall("(\w+-\w+)|(\w+'\w+)|(\w+-\w+'\w+)|(\w+)",line))        
-        sent+=len(re.findall("([A-ZА-ЯЁ][^\.!?]*[\.!?])",line))
-        picture+=len(re.findall('(<img src=""https://\w+""\s?>)',line))
-print(symbols,symbols-spaces,symbols-pun_sym,words,sent,picture)
+        line = ','.join(l)
+        symbols += len(line)
+        spaces += line.count(' ')
+        pun_sym += line.count('.') + line.count(',') + line.count('?')
+        pun_sym += line.count('!') + line.count('-') + line.count(':')
+        pun_sym += line.count(';') + line.count('(') + line.count(')')
+        words += len(re.findall("(\w+-\w+)|(\w+'\w+)|(\w+-\w+'\w+)|(\w+)", line))        
+        sent += len(re.findall("([A-ZА-ЯЁ][^\.!?]*[\.!?])", line))
+        picture += len(re.findall('(<img\ssrc[=]["]{1,}https[:][/]{2})', line))
+print(symbols, symbols-spaces, symbols-pun_sym, words, sent, picture)
