06a0c9081ed72c763b3054e810b329783c33a92f
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2020-12-09 01:17:30 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -4,7 +4,7 @@ symbols = 0
 spaces = 0
 pun_sym = 0
 words = 0
-sent = 0
+sentence = 0
 picture = 0
 with open('steam_description_data.csv', encoding='utf-8') as f:
     r=csv.reader(f)
@@ -16,6 +16,6 @@ with open('steam_description_data.csv', encoding='utf-8') as f:
         pun_sym += line.count('!') + line.count('-') + line.count(':')
         pun_sym += line.count(';') + line.count('(') + line.count(')')
         words += len(re.findall("(\w+-\w+)|(\w+'\w+)|(\w+-\w+'\w+)|(\w+)", line))        
-        sent += len(re.findall("([A-ZА-ЯЁ][^\.!?]*[\.!?])", line))
+        sentence += len(re.findall("([A-ZА-ЯЁ][^\.!?]*[\.!?])", line))
         picture += len(re.findall('(<img\ssrc[=]["]{1,}https[:][/]{2})', line))
-print(symbols, symbols-spaces, symbols-pun_sym, words, sent, picture)
+print(symbols, symbols-spaces, symbols-pun_sym, words, sentence, picture)
